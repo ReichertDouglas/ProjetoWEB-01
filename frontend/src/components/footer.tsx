22ca2f29418cb70e60e83709d7494c453ccfd854
ReichertDouglas <reichert_douglas@hotmail.com>
2025-09-04 22:17:53 -0300

    footer fix

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -3,7 +3,7 @@ const data = new Date()
 const ano = data.getFullYear()
 
     return(
-        <footer className="bg-gray-700 text-white text-center py-6 ">
+        <footer className="bg-blue-950 text-white text-center py-6 ">
             <p className="text-lg">
                 &copy; Todos direitos reservados.
                 &reg; Marca registrada
